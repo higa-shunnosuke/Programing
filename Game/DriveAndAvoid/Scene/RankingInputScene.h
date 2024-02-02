fba83f2fe24e198174e88750229268577fd7336d
higa-shunnosuke <138739372+higa-shunnosuke@users.noreply.github.com>
2024-02-02 15:06:28 +0900

    Utilityの追加
    Utilityの内容を追加しました。

--- Game/DriveAndAvoid/Scene/RankingInputScene.h
+++ Game/DriveAndAvoid/Scene/RankingInputScene.h
@@ -1,5 +1,25 @@
 #pragma once
-class RankingInputScene
+
+#include"SceneBase.h"
+#include"../Object/RankingData.h"
+
+class RankingInputScene:public SceneBase
 {
+private:
+	int background_image;	//
+	RankingData* ranking;	//
+	int score;				//
+	char name[15];			//
+	int name_num;			//
+	int cursor_x;			//
+	int cursor_y;			//
+
+public:
+	RankingInputScene();
+	virtual ~RankingInputScene();
+
+	virtual void Initialize() overrride;
+
+
 };
 
